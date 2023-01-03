843b1243126384cdc349e3ca276821fbe4422854
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-03 11:59:26 +0100

    partie 1

---
+++ public/js/main.js
@@ -0,0 +1,112 @@
+// EXO1
+
+// function addition(a, b = 1) {
+//     return a + b;
+// }
+
+// console.log(addition(5, 2));
+
+
+
+//EXO2
+
+// function soustraction(a, b = 1) {
+//     return a - b;
+// }
+
+// console.log(soustraction(5, 2));
+
+
+//EXO3
+
+// function multi(a, b = 1) {
+//     return a * b;
+// }
+
+// console.log(multi(5, 2));
+
+
+
+//EXO4
+
+// function divi(a, b = 1) {
+//     return a / b;
+// }
+
+// console.log(divi(5, 2));
+
+
+//EXO5
+
+// function addition(a, b = 1) {
+//     return a % b;
+// }
+
+// console.log(addition(5, 2));
+
+
+
+//EXO6
+// function addition(a, b = 1) {
+//     return a ** b;
+// }
+
+// console.log(addition(5, 2));
+
+
+
+//EXO7
+// function addition(a = 1) {
+//     return Math.sqrt(a);
+// }
+
+// console.log(addition(16));
+
+
+
+//EXO8
+
+// let uno = prompt("")
+
+// function capitalize(uno){
+//     return uno.charAt(0).toUpperCase()+uno.substr(1);
+// }
+
+// console.log(capitalize(uno));
+
+
+
+//EXO9
+
+//PAS ARRIVER
+
+// let nbr1= +prompt("")
+// let operator = prompt("")
+// let nbr2= +prompt("")
+
+
+
+// function calcul(nbr1, operator, nbr2) {
+
+//     switch (operator) {
+//             case "+":
+//                 console.log(nbr1 + nbr2);
+//                 break;
+        
+//             case "-":
+//                 console.log(nbr1 - nbr2);
+//                 break;
+        
+//             case "*":
+//                  console.log(nbr1 * nbr2);
+//                 break;
+        
+//             case "/":
+//                 console.log(nbr1 / nbr2);
+//             break;
+        
+        
+// }
+
+// }
+
